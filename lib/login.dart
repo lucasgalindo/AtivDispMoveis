5d776e17f5f743f1ecd2b443b7cfe559d62617a7
Lucas Galindo <lucasgalindomds@gmail.com>
2024-09-14 12:42:58 -0300

    first commit

---
+++ lib/login.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Login",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Meu App",
+          style: TextStyle(color: Colors.white),),
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(98, 1, 37, 116),
+        ),
+        body: Center(        
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 16.0),  
+                  child: 
+                  const Text("Bem Vindo!"),
+                ),
+                const Text("Sing In"),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
+                  child: 
+                const TextField(
+                  decoration: InputDecoration(
+                    labelText: 'User Name',
+                    contentPadding: EdgeInsets.all(8.0),
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 0),
+                  child: 
+                const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    contentPadding: EdgeInsets.all(8.0),
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 8.0),
+                  child: 
+                TextButton(onPressed: () {}, child: const Text("Forgot Password")),
+                ),
+
+                Container(
+                  child: ElevatedButton(onPressed:() {}, child: const Text("Login"),
+                  style: const ButtonStyle(padding: WidgetStatePropertyAll<EdgeInsets>(EdgeInsets.symmetric(horizontal: 64.0, vertical: 16.0)),
+                  shape: WidgetStatePropertyAll<OutlinedBorder>(LinearBorder()))
+                  ),
+                  ),
+                  
+      
+              ],
+            ),
+            ),
+          ),
+    );
+  }
+
+
+
+
+}
